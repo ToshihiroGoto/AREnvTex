95ff3ae4e2b56f7cc50af7e9c0f8db72bb2dca54
ToshihiroGotoh <t-goto@serialgames.co.jp>
2018-06-29 19:38:06 +0900

    Visible CubeMap

--- AREnvTex/ViewController.swift
+++ AREnvTex/ViewController.swift
@@ -13,7 +13,12 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
-    @IBOutlet weak var envTexture: UIImageView!
+    @IBOutlet weak var envTexture1: UIImageView!
+    @IBOutlet weak var envTexture2: UIImageView!
+    @IBOutlet weak var envTexture3: UIImageView!
+    @IBOutlet weak var envTexture4: UIImageView!
+    @IBOutlet weak var envTexture5: UIImageView!
+    @IBOutlet weak var envTexture6: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,9 +68,29 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     }
 */
     
-    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
-        let probe:AREnvironmentProbeAnchor = anchors.first as! AREnvironmentProbeAnchor
-        print(probe)
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        for case let envAnchor as AREnvironmentProbeAnchor in anchors {
+            let textureSlice1 = envAnchor.environmentTexture?.makeTextureView(pixelFormat: .rgba8Unorm, textureType: .type2D, levels: 0 ..< 1, slices: 0..<1)
+            let textureSlice2 = envAnchor.environmentTexture?.makeTextureView(pixelFormat: .rgba8Unorm, textureType: .type2D, levels: 0 ..< 1, slices: 1..<2)
+            let textureSlice3 = envAnchor.environmentTexture?.makeTextureView(pixelFormat: .rgba8Unorm, textureType: .type2D, levels: 0 ..< 1, slices: 2..<3)
+            let textureSlice4 = envAnchor.environmentTexture?.makeTextureView(pixelFormat: .rgba8Unorm, textureType: .type2D, levels: 0 ..< 1, slices: 3..<4)
+            let textureSlice5 = envAnchor.environmentTexture?.makeTextureView(pixelFormat: .rgba8Unorm, textureType: .type2D, levels: 0 ..< 1, slices: 4..<5)
+            let textureSlice6 = envAnchor.environmentTexture?.makeTextureView(pixelFormat: .rgba8Unorm, textureType: .type2D, levels: 0 ..< 1, slices: 5..<6)
+            
+            let ciImage1 = CIImage(mtlTexture: textureSlice1!, options: nil)
+            let ciImage2 = CIImage(mtlTexture: textureSlice2!, options: nil)
+            let ciImage3 = CIImage(mtlTexture: textureSlice3!, options: nil)
+            let ciImage4 = CIImage(mtlTexture: textureSlice4!, options: nil)
+            let ciImage5 = CIImage(mtlTexture: textureSlice5!, options: nil)
+            let ciImage6 = CIImage(mtlTexture: textureSlice6!, options: nil)
+            
+            envTexture1.image = UIImage(ciImage: ciImage1!)
+            envTexture2.image = UIImage(ciImage: ciImage2!)
+            envTexture3.image = UIImage(ciImage: ciImage3!)
+            envTexture4.image = UIImage(ciImage: ciImage4!)
+            envTexture5.image = UIImage(ciImage: ciImage5!)
+            envTexture6.image = UIImage(ciImage: ciImage6!)
+        }
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
@@ -83,3 +108,5 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         
     }
 }
+
+//-----
